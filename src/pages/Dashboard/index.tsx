dd984eb634aa895da259cbaa37aa3912506135d5
glaubermatos <glaub.oliveira@hotmail.com>
2021-11-07 01:28:45 -0300

    :tada: Challenge Finalized

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
 
-import Header from '../../components/Header';
 import api from '../../services/api';
+
+import { FoodFormData, FoodModel } from '../../types';
+
+import Header from '../../components/Header';
 import Food from '../../components/Food';
 import ModalAddFood from '../../components/ModalAddFood';
 import ModalEditFood from '../../components/ModalEditFood';
+
 import { FoodsContainer } from './styles';
-import { FoodFormData, FoodModel } from '../../types';
 
 export default function Dashboard() {
   const [foods, setFoods] = useState<FoodModel[]>([])
